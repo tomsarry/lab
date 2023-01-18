758a2b154f5438d90a84149299c7a5831e15488e
Tom Sarry <tom.sarry@mail.mcgill.ca>
2023-01-18 12:14:28 -0500

    chore: update tab space

--- 1/fd.sh
+++ 1/fd.sh
@@ -6,8 +6,8 @@
 
 if [ $# -ne 3 ];
 then
-    echo "fd: illegal number of parameters"
-    exit 1
+  echo "fd: illegal number of parameters"
+  exit 1
 fi
 
 count=0
@@ -16,24 +16,24 @@ start=$2
 end=$3
 
 if [ ! -f $filename ]; then
-    echo "fd: invalid file"
-    exit 1
+  echo "fd: invalid file"
+  exit 1
 fi
 
 re='^[0-9]+$'
 if [[ ! ($start =~ $re && $end =~ $re) ]]; then
-    echo "fd: illegal parameter types, expected integers"
-    exit 1
+  echo "fd: illegal parameter types, expected integers"
+  exit 1
 fi
 
 while read -r line; do
-    ((count++))
-    
-    if [ $count -lt $start ]; then
-        continue
-        elif [ $count -gt $end ]; then
-        break
-    fi
-    
-    echo $line
+  ((count++))
+  
+  if [ $count -lt $start ]; then
+    continue
+    elif [ $count -gt $end ]; then
+    break
+  fi
+  
+  echo $line
 done < $filename
