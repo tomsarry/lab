b5d76f6da2347a36668ea4431f60cca7dc035a97
Tom Sarry <tom.sarry@mail.mcgill.ca>
2023-01-19 15:19:53 -0500

    feat: add lc

---
+++ 4/create_files.sh
@@ -0,0 +1,9 @@
+touch a1.txt
+
+printf "a\nb\n" >> a2.txt
+
+for c in {1,2,3,4,5,6,7,8,9}; do
+  echo $c >> a3.txt
+done
+
+mkdir a4 a5
---
+++ 4/lc.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+# Usage:
+#   lc <file1|dir1> [<file2|dir2> ...]
+#   Specifies for each argument if it is a directory, or a file.
+#   For files, also provides the number of lines.
+
+if [ $# -lt 1 ]; then
+  echo "$0: illegal number of parameters"
+  exit 1
+fi
+
+for filename in $@; do
+  if [[ -f $filename ]]; then
+    lines=`cat $filename | wc -l`
+    echo "- file: $filename, $lines lines"
+    elif [[ -d $filename ]]; then
+    echo "- directory $filename"
+  fi
+done
